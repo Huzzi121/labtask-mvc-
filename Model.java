664ebca4e4790e132a97eb6fed0b1a38a31cfe23
Huzzi121 <136900052+Huzzi121@users.noreply.github.com>
2024-12-03 11:49:35 +0500

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,53 @@
+package sda_Model_Veiw_Controller;
+
+public class Model {
+    private String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
+
+class View {
+    public void printData(String data) {
+        System.out.println("Data: " + data);
+    }
+}
+
+class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setData(String data) {
+        model.setData(data);
+    }
+
+    public String getData() {
+        return model.getData();
+    }
+
+    public void updateView() {
+        view.printData(model.getData());
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        // Create the model, view, and controller
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller(model, view);
+        // Set the data and update the view
+        controller.setData("Hello, MKC!");
+        controller.updateView();
+    }
+}
